7fd1ac4c7b19c25bdee019d21402e22f9678c0e7
Shinoooooooooo <107863526+NamKhagg@users.noreply.github.com>
2022-08-03 14:04:16 +0700

    Nam Khang commit

---
+++ Front End/src/data.js
@@ -0,0 +1,150 @@
+export const sliderItems = [{
+        id: 1,
+        img: "https://static2.yan.vn/YanNews/2167221/201908/iu-la-ai-tinh-yeu-su-nghiep-bai-hat-cua-iu-4bb930f2.jpg",
+        title: "SUMMER SALE",
+        description: "DON'T COMPROMIS ON STYLE! GET PLAT 30% OFF FOR  NEW ARRIVALS",
+        bg: "fcf1ed"
+    },
+
+    {
+        id: 2,
+        img: "https://staticg.sportskeeda.com/editor/2022/05/135a5-16537516374304-1920.jpg",
+        title: "FALL SALE",
+        description: "DON'T COMPROMIS ON STYLE! GET PLAT 30% OFF FOR  NEW ARRIVALS",
+        bg: "faf5fd"
+    },
+
+    {
+        id: 3,
+        img: "https://kenh14cdn.com/203336854389633024/2021/2/13/3l1cytk-16132193585051290517086.jpg",
+        title: "WINTER SALE",
+        description: "DON'T COMPROMIS ON STYLE! GET PLAT 30% OFF FOR  NEW ARRIVALS",
+        bg: "fcf1ed"
+    },
+
+    {
+        id: 4,
+        img: "https://static2.yan.vn/YanNews/2167221/201908/iu-la-ai-tinh-yeu-su-nghiep-bai-hat-cua-iu-4bb930f2.jpg",
+        title: "SPRING SALE",
+        description: "DON'T COMPROMIS ON STYLE! GET PLAT 30% OFF FOR  NEW ARRIVALS",
+        bg: "fcf1eg"
+    }
+]
+
+export const categories = [{
+        id: 1,
+        img: "https://kenh14cdn.com/thumb_w/660/203336854389633024/2022/4/28/20220322141738-1651118648724855658012.jpg",
+        title: "Elegant OutFit",
+        cat: "elegant"
+    },
+    {
+        id: 2,
+        img: "https://icdn.dantri.com.vn/thumb_w/770/2022/05/30/iu-11-1653887051969.jpg",
+        title: "Sexy OutFit",
+        cat: "sexy"
+    },
+    {
+        id: 3,
+        img: "https://2sao.vietnamnetjsc.vn/images/2022/02/20/21/00/IU-2.jpg",
+        title: "Sport OutFit",
+        cat: "sport"
+    },
+    {
+        id: 4,
+        img: "https://i.bloganchoi.com/bloganchoi.com/wp-content/uploads/2022/03/iu-beauty.jpg?fit=564%2C20000&quality=95&ssl=1",
+        title: "Lady OutFit",
+        cat: "lady"
+    },
+    {
+        id: 5,
+        img: "https://afamilycdn.com/150157425591193600/2022/3/22/2755123182910519797454857821543180882404446n-1647948033438996413898.jpg",
+        title: "Nàng thơ OutFit",
+        cat: "naive"
+    }
+]
+
+// export const popularProduct = [{
+//         id: 1,
+//         img: "https://toppng.com/uploads/preview/white-back-png-stickpng-white-t-shirt-back-11563034052wuag1pj3ur.png"
+//     },
+
+//     {
+//         id: 2,
+//         img: "https://i.pinimg.com/736x/2e/88/a5/2e88a52be17ccbce0306aae0a295138c.jpg"
+//     },
+
+//     {
+//         id: 3,
+//         img: "https://toppng.com/uploads/preview/white-back-png-stickpng-white-t-shirt-back-11563034052wuag1pj3ur.png"
+//     },
+
+//     {
+//         id: 4,
+//         img: "https://i.pinimg.com/736x/2e/88/a5/2e88a52be17ccbce0306aae0a295138c.jpg"
+//     },
+
+//     {
+//         id: 5,
+//         img: "https://toppng.com/uploads/preview/white-back-png-stickpng-white-t-shirt-back-11563034052wuag1pj3ur.png"
+//     },
+
+//     {
+//         id: 6,
+//         img: "https://i.pinimg.com/736x/2e/88/a5/2e88a52be17ccbce0306aae0a295138c.jpg"
+//     },
+
+//     {
+//         id: 7,
+//         img: "https://toppng.com/uploads/preview/white-back-png-stickpng-white-t-shirt-back-11563034052wuag1pj3ur.png"
+//     },
+
+//     {
+//         id: 8,
+//         img: "https://i.pinimg.com/736x/2e/88/a5/2e88a52be17ccbce0306aae0a295138c.jpg"
+//     },
+
+//     {
+//         id: 9,
+//         img: "https://toppng.com/uploads/preview/white-back-png-stickpng-white-t-shirt-back-11563034052wuag1pj3ur.png"
+//     },
+
+//     {
+//         id: 10,
+//         img: "https://i.pinimg.com/736x/2e/88/a5/2e88a52be17ccbce0306aae0a295138c.jpg"
+//     }
+
+// ]
+
+export const popularProduct = [{
+        id: 1,
+        img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+        id: 2,
+        img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+        id: 3,
+        img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+        id: 4,
+        img: "https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+    },
+    {
+        id: 5,
+        img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+        id: 6,
+        img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+        id: 7,
+        img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+        id: 8,
+        img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+]
