7c70866198505cb76df079cc533518cab2bc5e8b
NoNameNo1F <NoNameNo1F@gmail.com>
2022-08-06 13:09:19 +0700

    [NameVu] add: List New User

---
+++ Front End/admin/src/components/widgetSm/WidgetSm.jsx
@@ -0,0 +1,101 @@
+import "./widgetSm.css";
+import { Visibility } from "@mui/icons-material";
+export default function WidgetSm() {
+  return (
+    <div className="widgetSm">
+      <span className="widgetSmTitle">New Join Members</span>
+      <ul className="widgetSmList">
+        <li className="widgetSmListItem">
+          <img
+            src="http://robonwriting.com/wp-content/uploads/2013/04/no-avatar-male.jpg"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Vu Nguyen Nam Cao</span>
+            <span className="widgetSmUserTitle">Backend Dev</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon" />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmListItem">
+          <img
+            src="http://robonwriting.com/wp-content/uploads/2013/04/no-avatar-male.jpg"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Vu Nguyen Nam Cao</span>
+            <span className="widgetSmUserTitle">Backend Dev</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon" />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmListItem">
+          <img
+            src="http://robonwriting.com/wp-content/uploads/2013/04/no-avatar-male.jpg"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Vu Nguyen Nam Cao</span>
+            <span className="widgetSmUserTitle">Backend Dev</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon" />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmListItem">
+          <img
+            src="http://robonwriting.com/wp-content/uploads/2013/04/no-avatar-male.jpg"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Vu Nguyen Nam Cao</span>
+            <span className="widgetSmUserTitle">Backend Dev</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon" />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmListItem">
+          <img
+            src="http://robonwriting.com/wp-content/uploads/2013/04/no-avatar-male.jpg"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Vu Nguyen Nam Cao</span>
+            <span className="widgetSmUserTitle">Backend Dev</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon" />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmListItem">
+          <img
+            src="http://robonwriting.com/wp-content/uploads/2013/04/no-avatar-male.jpg"
+            alt=""
+            className="widgetSmImg"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUsername">Vu Nguyen Nam Cao</span>
+            <span className="widgetSmUserTitle">Backend Dev</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon" />
+            Display
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+}
