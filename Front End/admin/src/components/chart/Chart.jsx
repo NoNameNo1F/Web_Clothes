f4a99b811c5ffd46274fc492acc95fc422b32d73
NoNameNo1F <NoNameNo1F@gmail.com>
2022-08-06 13:02:52 +0700

    [NameVu] add: chart component

---
+++ Front End/admin/src/components/chart/Chart.jsx
@@ -0,0 +1,26 @@
+import "./chart.css";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+import { Category } from "@mui/icons-material";
+export default function Chart({ title, data, dataKey, grid }) {
+  return (
+    <div className="chart">
+      <h3 className="charTitle">{title} </h3>
+      <ResponsiveContainer width="100%" aspect={4 / 1}>
+        <LineChart data={data}>
+          <XAxis dataKey="name" stroke="#191645" />
+          <Line type="monotone" dataKey={dataKey} stroke="#43C6AC" />
+          <Tooltip />
+          {grid && <CartesianGrid stroke="#191645" strokeDasharray="5 5" />}
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
