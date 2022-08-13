6473fdf184c1c99992611e9c5d66bc6fb62d60aa
NoNameNo1F <NoNameNo1F@gmail.com>
2022-08-13 11:15:02 +0700

    [NameVu] adjust: add key: img

--- Back End/model/User.js
+++ Back End/model/User.js
@@ -21,7 +21,8 @@ const userSchema = new mongoose.Schema({
     isAdmin: {
         type: Boolean,
         default: false
-    }
+    },
+    img: { type: String }
 }, { timestamps: true });
 
 module.exports = mongoose.model("User", userSchema);
