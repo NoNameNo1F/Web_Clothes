53c614ba28ab0daa15efb0028bd3a376064ff221
NoNameNo1F <NoNameNo1F@gmail.com>
2022-08-13 12:08:33 +0700

    [NameVu] add: dispatch /product, auth/login

--- Front End/admin/src/redux/apiCalls.js
+++ Front End/admin/src/redux/apiCalls.js
@@ -1,5 +1,19 @@
 import { loginStart, loginFailure, loginSuccess } from "./userRedux";
-import { publicRequest } from '../requestMethods';
+import { publicRequest, userRequest } from '../requestMethods';
+import {
+    getProductFailure,
+    getProductStart,
+    getProductSuccess,
+    deleteProductFailure,
+    deleteProductStart,
+    deleteProductSuccess,
+    updateProductFailure,
+    updateProductStart,
+    updateProductSuccess,
+    addProductFailure,
+    addProductStart,
+    addProductSuccess,
+} from "./productRedux";
 
 export const login = async (dispatch, user) => {
     dispatch(loginStart());
@@ -11,3 +25,59 @@ export const login = async (dispatch, user) => {
     }
 };
 
+//GET Product list
+export const getProducts = async (dispatch) => {
+    dispatch(getProductStart());
+    try {
+        const res = await publicRequest.get('/product');
+        dispatch(getProductSuccess(res.data));
+    } catch (err) {
+        dispatch(getProductFailure());
+    }
+};
+
+export const deleteProduct = async (id, dispatch) => {
+    dispatch(deleteProductStart());
+    try {
+        // const res = await userRequest.delete(`/products/${id}`);
+        dispatch(deleteProductSuccess(id));
+    } catch (err) {
+        dispatch(deleteProductFailure());
+    }
+};
+
+export const updatteProduct = async (id, product, dispatch) => {
+    dispatch(updateProductStart());
+    try {
+        dispatch(updateProductSuccess({ id, product }));
+    } catch (err) {
+        dispatch(updateProductFailure());
+    }
+}
+
+export const addProduct = async (product, dispatch) => {
+    dispatch(addProductStart());
+    try {
+        const res = await userRequest.post('/product', product);
+        dispatch(addProductSuccess(res.data));
+    } catch (err) {
+        dispatch(addProductFailure());
+    }
+}
+// export const addProduct = async (product, dispatch) => {
+//     dispatch(addProductStart());
+//     try {
+//         const res = await userRequest({
+//             method: 'POST',
+//             url: '/product',
+//             headers: {
+//                 token: `Bearer ${localStorage.getItem('token')}`
+//             },
+//             data: product
+
+//         })
+//         dispatch(addProductSuccess(res.data));
+//     } catch (err) {
+//         dispatch(addProductFailure());
+//     }
+// }
