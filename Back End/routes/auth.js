656bb5c6ac8e50a94e919bab9bf37407726a620e
NoNameNo1F <NoNameNo1F@gmail.com>
2022-08-13 11:17:27 +0700

    [NameVu] fix: fix login failed ->crash server

--- Back End/routes/auth.js
+++ Back End/routes/auth.js
@@ -41,7 +41,7 @@ router.post("/login", async (req, res) => {
 
         res.status(200).json({ ...others, accessToken });
     } catch (error) {
-        res.status(500).json(error);
+        //res.status(500).json(error);
     }
 });
 
