f0107abfdb3d323ba7b71a7a94a8595cac357930
NoNameNo1F <NoNameNo1F@gmail.com>
2022-08-13 11:45:39 +0700

    [NameVu] add add useEffect

--- Front End/admin/src/components/featuredInfo/FeaturedInfo.jsx
+++ Front End/admin/src/components/featuredInfo/FeaturedInfo.jsx
@@ -1,14 +1,36 @@
 import "./featuredInfo.css";
 import { ArrowDownward, ArrowUpward } from "@mui/icons-material";
+import { useEffect, useState } from "react";
+import { userRequest } from "../../requestMethods";
+
 export default function FeaturedInfo() {
+  const [income, setIncome] = useState([]);
+  const [perc, setPerc] = useState(0);
+
+  useEffect(() => {
+    const getIncome = async () => {
+      try {
+        const res = await userRequest.get("orders/income");
+        setIncome(res.data);
+        setPerc((res.data[1].total * 100) / res.data[0].total - 100);
+      } catch {}
+    };
+    getIncome();
+  }, []);
+
   return (
     <div className="featured">
       <div className="featuredItem">
         <span className="featuredTitle">Revanue</span>
         <div className="featuredMoneyContainer">
-          <span className="featuredMoney">$2,415</span>
+          <span className="featuredMoney">${income[1]?.total}</span>
           <span className="featuredMoneyRate">
-            -11,4 <ArrowDownward className="featuredIcon negative" />
+            %{Math.floor(perc)}{" "}
+            {perc < 0 ? (
+              <ArrowDownward className="featuredIcon negative" />
+            ) : (
+              <ArrowUpward className="featuredIcon" />
+            )}
           </span>
         </div>
         <span className="featuredSub">Compared to last month</span>
