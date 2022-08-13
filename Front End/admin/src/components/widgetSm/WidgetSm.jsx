6cda6f6ffe313e69b3832c0056ea4fe85dad8595
NoNameNo1F <NoNameNo1F@gmail.com>
2022-08-13 11:48:52 +0700

    [NameVu] add useEffect on New Join member

--- Front End/admin/src/components/widgetSm/WidgetSm.jsx
+++ Front End/admin/src/components/widgetSm/WidgetSm.jsx
@@ -1,100 +1,42 @@
 import "./widgetSm.css";
 import { Visibility } from "@mui/icons-material";
+import { useEffect, useState } from "react";
+import { userRequest } from "../../requestMethods";
 export default function WidgetSm() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const res = await userRequest.get("users/?new=true");
+        setUsers(res.data);
+      } catch {}
+    };
+    getUsers();
+  }, []);
   return (
     <div className="widgetSm">
       <span className="widgetSmTitle">New Join Members</span>
       <ul className="widgetSmList">
-        <li className="widgetSmListItem">
-          <img
-            src="http://robonwriting.com/wp-content/uploads/2013/04/no-avatar-male.jpg"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Vu Nguyen Nam Cao</span>
-            <span className="widgetSmUserTitle">Backend Dev</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
-            Display
-          </button>
-        </li>
-        <li className="widgetSmListItem">
-          <img
-            src="http://robonwriting.com/wp-content/uploads/2013/04/no-avatar-male.jpg"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Vu Nguyen Nam Cao</span>
-            <span className="widgetSmUserTitle">Backend Dev</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
-            Display
-          </button>
-        </li>
-        <li className="widgetSmListItem">
-          <img
-            src="http://robonwriting.com/wp-content/uploads/2013/04/no-avatar-male.jpg"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Vu Nguyen Nam Cao</span>
-            <span className="widgetSmUserTitle">Backend Dev</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
-            Display
-          </button>
-        </li>
-        <li className="widgetSmListItem">
-          <img
-            src="http://robonwriting.com/wp-content/uploads/2013/04/no-avatar-male.jpg"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Vu Nguyen Nam Cao</span>
-            <span className="widgetSmUserTitle">Backend Dev</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
-            Display
-          </button>
-        </li>
-        <li className="widgetSmListItem">
-          <img
-            src="http://robonwriting.com/wp-content/uploads/2013/04/no-avatar-male.jpg"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Vu Nguyen Nam Cao</span>
-            <span className="widgetSmUserTitle">Backend Dev</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
-            Display
-          </button>
-        </li>
-        <li className="widgetSmListItem">
-          <img
-            src="http://robonwriting.com/wp-content/uploads/2013/04/no-avatar-male.jpg"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Vu Nguyen Nam Cao</span>
-            <span className="widgetSmUserTitle">Backend Dev</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
-            Display
-          </button>
-        </li>
+        {users.map((user) => {
+          <li className="widgetSmListItem" key={user._id}>
+            <img
+              src={
+                user.img ||
+                "https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+              }
+              alt=""
+              className="widgetSmImg"
+            />
+            <div className="widgetSmUser">
+              <span className="widgetSmUsername">{user.username}</span>
+            </div>
+            <button className="widgetSmButton">
+              <Visibility className="widgetSmIcon" />
+              Display
+            </button>
+          </li>;
+        })}
       </ul>
     </div>
   );
