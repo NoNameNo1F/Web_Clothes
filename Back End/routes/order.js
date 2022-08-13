4081e3deffa47dece6479356a82cb571092fddfb
NoNameNo1F <NoNameNo1F@gmail.com>
2022-08-13 11:18:50 +0700

    [NameVu] add: income route

--- Back End/routes/order.js
+++ Back End/routes/order.js
@@ -61,41 +61,41 @@ router.get('/', verifyTokenAndAdmin, async (req, res) => {
     }
 });
 
-// GET MONTHLY INCOME
-// router.get('/income',verifyTokenAndAdmin, async (req, res) => {
-//     const date = new Date();
-//     const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
-//     const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
+//GET MONTHLY INCOME
+router.get('/income', verifyTokenAndAdmin, async (req, res) => {
+    const date = new Date();
+    const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+    const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
 
-//     try{
-//         const income = await Order.aggregate([
-//             {
-//                 $match: {
-//                     createdAt: {
-//                         $gte: previousMonth
-//                     }
-//                 }
-//             },
-//             {
-//                 $project: {
-//                     month: {
-//                         $month: '$createdAt'
-//                     },
-//                     sales: '$amount'
-//                 }
-//             },
-//             {
-//                 $group: {
-//                     _id: '$month',
-//                     total: {
-//                         $sum: '$sales'
-//                     }
-//                 }
-//             }
-//         ]);
-//         res.status(200).json(income);
-//     }catch(err) {
-//         res.status(500).json(err);
-//     }
-// });
+    try {
+        const income = await Order.aggregate([
+            {
+                $match: {
+                    createdAt: {
+                        $gte: previousMonth
+                    }
+                }
+            },
+            {
+                $project: {
+                    month: {
+                        $month: '$createdAt'
+                    },
+                    sales: '$amount'
+                }
+            },
+            {
+                $group: {
+                    _id: '$month',
+                    total: {
+                        $sum: '$sales'
+                    }
+                }
+            }
+        ]);
+        res.status(200).json(income);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 module.exports = router;
