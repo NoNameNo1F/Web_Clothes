3aa18f551da4a9c9d10bbd2f46ce1536abede27a
NoNameNo1F <NoNameNo1F@gmail.com>
2022-08-13 12:16:16 +0700

    [NameVu] add: config firebase cloud to store image

---
+++ Front End/admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDQAITUG4OX6JHxQ1RCLjFpTUB279xUZ8k",
+    authDomain: "shopeefake-e854b.firebaseapp.com",
+    projectId: "shopeefake-e854b",
+    storageBucket: "shopeefake-e854b.appspot.com",
+    messagingSenderId: "970634970357",
+    appId: "1:970634970357:web:56dea184c989c4cf3071bc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
