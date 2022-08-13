cdbd91afb2ca10971f8f7e99e1a166923c96a29b
NoNameNo1F <NoNameNo1F@gmail.com>
2022-08-13 12:05:02 +0700

    [NameVu] add useEffect on Product page

--- Front End/admin/src/pages/product/Product.jsx
+++ Front End/admin/src/pages/product/Product.jsx
@@ -1,9 +1,58 @@
 import "./product.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Chart from "../../components/chart/Chart";
-import { productData } from "../../dummyData";
 import { Publish } from "@mui/icons-material";
+import { useSelector } from "react-redux";
+import { useEffect, useMemo, useState } from "react";
+import { userRequest } from "../../requestMethods";
+
 export default function Product() {
+  const location = useLocation();
+  const productId = location.pathname.split("/")[2];
+  const [pStats, setPStats] = useState([]);
+
+  const product = useSelector((state) =>
+    state.product.products.find((product) => product._id === productId)
+  );
+
+  const MONTHS = useMemo(
+    () => [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Agu",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    []
+  );
+
+  useEffect(() => {
+    const getStats = async () => {
+      try {
+        const res = await userRequest.get("orders/income?pid=" + productId);
+        const list = res.data.sort((a, b) => {
+          return a._id - b._id;
+        });
+        list.map((item) =>
+          setPStats((prev) => [
+            ...prev,
+            { name: MONTHS[item._id - 1], Sales: item.total },
+          ])
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getStats();
+  }, [productId, MONTHS]);
+
   return (
     <div className="product">
       <div className="productTitleContainer">
@@ -14,33 +63,32 @@ export default function Product() {
       </div>
       <div className="productTop">
         <div className="productTopLeft">
-          <Chart data={productData} dataKey="Sales" title="Sales Performance" />
+          <Chart data={pStats} dataKey="Sales" title="Sales Performance" />
         </div>
         <div className="productTopRight">
           <div className="productInfoTop">
             <img
               className="productInfoImg"
-              src="https://media.vov.vn/sites/default/files/styles/large/public/2021-08/a10_0.jpg"
+              src={
+                product.img ||
+                "https://upload.wikimedia.org/wikipedia/commons/c/c0/Nothing.jpg"
+              }
               alt=""
             />
-            <span className="productName">Cat not buzy</span>
+            <span className="productName">{product.title}</span>
           </div>
           <div className="productInfoBottom">
             <div className="productInfoItem">
               <span className="productInfoKey">id:</span>
-              <span className="productInfoValue">1</span>
+              <span className="productInfoValue">{product._id}</span>
             </div>
             <div className="productInfoItem">
               <span className="productInfoKey">sales:</span>
               <span className="productInfoValue">1123</span>
             </div>
             <div className="productInfoItem">
-              <span className="productInfoKey">active:</span>
-              <span className="productInfoValue">yes</span>
-            </div>
-            <div className="productInfoItem">
               <span className="productInfoKey">in stock:</span>
-              <span className="productInfoValue">no</span>
+              <span className="productInfoValue">{product.inStock}</span>
             </div>
           </div>
         </div>
@@ -49,25 +97,20 @@ export default function Product() {
         <form className="productForm">
           <div className="productFormLeft">
             <label>Product Name </label>
-            <input type="text" placeholder="Cat not buzy" />
+            <input type="text" placeholder={product.title} />
+            <label>Product Description</label>
+            <input type="text" placeholder={product.desc} />
+            <label>Price</label>
+            <input type="text" placeholder={product.price} />
             <label>In Stock</label>
             <select name="inStock" id="inStock">
-              <option value="yes">Yes</option>
-              <option value="no">No</option>
-            </select>
-            <label>Active</label>
-            <select name="active" id="active">
-              <option value="yes">Yes</option>
-              <option value="no">No</option>
+              <option value="true">Yes</option>
+              <option value="false">No</option>
             </select>
           </div>
           <div className="productFormRight">
             <div className="productUpload">
-              <img
-                className="productUploadImg"
-                src="https://media.vov.vn/sites/default/files/styles/large/public/2021-08/a10_0.jpg"
-                alt=""
-              />
+              <img className="productUploadImg" src={product.img} alt="" />
               <label for="file">
                 <Publish />
               </label>
